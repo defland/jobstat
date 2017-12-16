a2564e5c4753dfa2b02ed19c8e64c7167963941f
“defkim” <yanggan@yeah.net>
2017-12-16 14:33:38 +0800

    added: flask project file

---
+++ frontend/instance/config.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SECRET_KEY = 'Sm9obiBTY2hyb20ga2lja3MgYXNz'
+STRIPE_API_KEY = 'SmFjb2IgS2FwbGFuLU1vc3MgaXMgYSBoZXJv'
+SQLALCHEMY_DATABASE_URI= \
+"postgresql://user:TWljaGHFgiBCYXJ0b3N6a2lld2ljeiEh@localhost/databasename"
--- frontend/runserver.py
+++ frontend/runserver.py
@@ -1,1 +1,12 @@
-#coding:utf-8
+# !/usr/bin/python
+# coding:utf-8
+from application import app
+
+
+if __name__=='__main__':
+    app.run(\
+        host=app.config['HOST'],
+        port=app.config['PORT'],
+        debug=app.config['DEBUG']
+        )
+
